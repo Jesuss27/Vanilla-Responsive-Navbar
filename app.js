451c52bbba8374dd503da77a7536f0de6f23ecb1
Jesus <65581378+Jesuss27@users.noreply.github.com>
2021-11-08 16:39:08 -0800

    Javascript programming

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 const menu = document.querySelector("#mobile-menu");
-const menuLinks = document.querySelector(".nav-links");
+const navMenu = document.querySelector(".nav-menu");
+const mobileMenu = document.querySelector("#mobile-menu")
+const navLinks = document.querySelectorAll(".nav-menu li a")
+
+menu.addEventListener("click", () => { 
+    navMenu.classList.toggle("active");
+    mobileMenu.classList.toggle("is-active");
+
+    navLinks.forEach((link, index) => {
+        if(link.style.animation){
+            link.style.animation = "";
+        } else {
+            link.style.animation = `navLinkFade 2s ease forwards ${index / 7 }s`
+        }
+    })
+})
